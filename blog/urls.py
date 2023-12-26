cdb2510e1d1d075cac62112684738a2e9bc7ebd9
Your Name <you@example.com>
2023-12-26 08:46:07 +0000

    add iot page.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('post/', views.post_list, name='post_list')
+    path('iot/', views.iot, name='iot')
     ]
