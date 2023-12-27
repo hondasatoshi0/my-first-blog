3c7754fba63eab4a215e72e530c246858aed3a95
Your Name <you@example.com>
2023-12-27 01:00:41 +0000

    post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .model import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts':posts})
 
 def iot(request):
     return render(request, 'blog/iot.html',{})
