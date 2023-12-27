e91d0ef2b168be49741a723b4ad5402147c2cc7f
Your Name <you@example.com>
2023-12-27 05:17:10 +0000

    2023-12-27

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
 
 def post_detail(request, pk):
@@ -19,7 +18,6 @@ def post_new(request):
         if form.is_valid(): # フォームの値が有効である場合、
             post = form.save(commit=False) # フォームに入力された値を一時格納（commit=Falseはモデルとして保存していないという意
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,12 +31,26 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
 
 def iot(request):
     return render(request, 'blog/iot.html',{})
